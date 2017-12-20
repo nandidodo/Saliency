acf6f4c557a590b51ea776b0eeb6e02b08365ab6
Beren <satainvicta@gmail.com>
2017-12-20 11:14:06 +0000

    Add files via upload

---
+++ Scripts/activate_script.sh
@@ -0,0 +1,7 @@
+#a quick script for activating our environment, andpossibly doing anything else random
+#!bin/bash
+
+cd
+cd /work/dlenv
+source bin/activate
+echo "python virtual environment activated"
