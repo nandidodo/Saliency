939e2021c9c724f8071d2b691857017f7521c9a9
Beren <satainvicta@gmail.com>
2017-12-20 11:07:48 +0000

    added default crop size functionality

--- Scripts/dataset_scripts.py
+++ Scripts/dataset_scripts.py
@@ -6,8 +6,15 @@ import sys
 #add our prev folder to the path
 sys.path.append('/home/beren/work/phd/saliency')
 from file_reader import *
+
+crop_size = (100,100)
 if len(sys.argv) ==2:
 	dataset_name = sys.argv[1]
+
+if len(sys.argv) == 3:
+	dataset_name = sys.argv[1]
+	crop_size = (sys.argv[2], sys.argv[2])
+
 
 
 
@@ -16,9 +23,9 @@ def save_MIT_dataset():
 	fixmaps = basepath+"/ALLFIXATIONMAPS"
 	stimuli = basepath+"/ALLSTIMULI"
 	#save fixations
-	save_images_per_directory(fixmaps, save_dir=basepath + "_fixation_maps")
+	save_images_per_directory(fixmaps,crop_size = crop_size, save_dir=basepath + "_fixation_maps")
 	#save images
-	save_images_per_directory(stimuli, save_dir=basepath + "_images")
+	save_images_per_directory(stimuli,crop_size = crop_size, save_dir=basepath + "_images")
 
 
 
