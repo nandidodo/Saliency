60d5ac9ba5a5bc67741bce2d6bdb1a3958488b73
Beren <satainvicta@gmail.com>
2018-01-11 10:08:26 +0000

    updated CVAE to be slightly better

--- gestalt/CVAE.py
+++ gestalt/CVAE.py
@@ -50,6 +50,12 @@ epochs = 100
 
 # okay, let's define our model
 
+# okay, so the variational autoeencoder consists of two parts - firstly the encoder which is a mapping/NN representation of the probability distribution p(z|X) i.e. it maps from the input space X to the latent variable space Z. we want to maximise the usefulness ot this thing and also how closely it approximates the standard normal N(0,1) as regularisation
+# secondly we want to map in the decoder from the latent variable to another X X^ and therefore maximise p(X|z) to match the data which is a stanard log thing, so I don't know
+# we'll generally want a much more dimensional latent variable, and I'm not sure how I shuold get it tbh
+# like the mu and sigma are just meant to be a standard normal vector, right? in multiple dimensions
+# so we should be able to deal with that, but we can't, so what do we do there?
+
 X = Input(shape=(n_x,))
 label = Input(shape=(n_y,))
 #now we concatenate x and y so we can igure this out
@@ -61,8 +67,42 @@ mu = Dense(n_z, activation='linear')(encoder_h)
 l_sigma = Dense(n_z, activation='linear')(encoder_h) # this is our mapping t omu and sigma via ANNs
 
 #now we have a function which add random noise to our sampling process, and use a lambda layer in keras for this - cool!
+# this is the reparametrization trick. basicaly, we're meant to be sampling from our calculated z normal N(mu, sigma) which are the outputs of the neural network, but this isn't differentiable, so what we do is instead sample from the standard normal, which isn't differentiable, but this doesn't matter as it's not actually dependent on anything in the network or trainable, so it doesn't need to ahve gradients, and then we multiply and add the other bits, which are differentiable, so the reparametrisation works, and the network is differentiable through these things
+
 def sample_z(args):
 	mu, l_sigma = args
 	eps = K.random_normal
+	return mu + K.exp(log_sigma/2) * eps
+
+# we use a lambda layer to sample our standard thing
+z = Lambda(sample_z)([mu, l_sigma])
+	#
+#now we build a very simple decoder network
+
+decoder_hidden = Dense(512, activation='relu')
+decoder_out = Dense(284, activation='sigmoid')
+h_p = decoder_hidden(z)
+outputs = decoder_out(h_p)
+
+# so we now have three things we can do, reconstruct inputs, encoder inputs into latentvariables
+# and generate data from latent variables. so let's look at this
+vae = Model(inputs, outputs)
+encoder = Model(inputs, mu) # we use mean ot the output as it'sthe center point, the representative of the gaussian
+
+# and we can also have a generator ourputs
+d_in = Input(shape=(n_z,))
+d_h = decoder_hidden(d_in)
+d_out = decoder_out(d_h)
+decoder = Model(d_in, d_out)
+
+# we also need to have our custom vae loss combining both the KL and the reconstruction loss
+
+def vae_loss(y_true, y_pred):
+	recon = K.sum(K.binary_crossentropy(y_pred, y_true), axis=1)
+	kl= 0.5 * K.sum(K.exp(log_sigma) + K.square(mu) - 1. - log_sigma, axis=1)
+	return recon + kl
 
 
+# and then we train it
+vae.compile(optimizer='adam', loss=vae_loss)
+vae.fit(xtrain, xtrain, batch_size=m, epochs=epochs)
