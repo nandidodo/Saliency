ceaba06aa6d9c17beedba32540fd7ab527f0ebc8
Beren <satainvicta@gmail.com>
2018-01-13 22:07:13 +0000

    Add files via upload

--- gestalt_models.py
+++ gestalt_models.py
@@ -89,6 +89,32 @@ def SimpleConvDropoutBatchNorm(input_shape):
 	model = Model(input_img, x)
 	return model
 
+def SimpleSequentialModel(input_shape):
+	#this is just a test of a sequential version of the conv net batch norm
+	#hopefully it will work?
+	model=Sequential()
+	model.add(Conv2D(64,2,strides=(1,1),activation='relu',padding='same', input_shape=input_shape))
+	model.add(BatchNormalization(momentum=0.9))
+	model.add(Conv2D(64,2,strides=(2,2), activation='relu', padding='same'))
+	model.add(BatchNormalization(momentum=0.9))
+	model.add(Conv2D(32,1,strides=(1,1), activation='relu', padding='same'))
+	model.add(BatchNormalization(momentum=0.9))
+	model.add(MaxPooling2D((2,2), strides=(2,2)))
+	model.add(BatchNormalization(momentum=0.9))
+	model.add(Dropout(0.1))
+	print "ENCODER COMPLETE"
+
+	#DECODER
+	model.add(Conv2DTranspose(64,2,strides=(2,2), activation='relu',padding='same'))
+	model.add(BatchNormalization(momentum=0.9))
+	model.add(Conv2DTranspose(32,1,strides=(1,1),activation='relu', padding='same'))
+	model.add(BatchNormalization(momentum=0.9))
+	model.add(Conv2DTranspose(1, 2,strides=(2,2),activation='relu',padding='valid'))
+	print "DECODER COMPLETE"
+	model.summary()
+	return model
+	
+
 def SimpleAutoencoder(input_shape, weights_path=None):
 	
 	input_img = Input(input_shape)
