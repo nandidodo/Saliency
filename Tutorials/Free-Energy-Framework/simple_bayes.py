3df4e6a3b0781de77fb4fdda81f4ea96e77ae9b9
Beren <satainvicta@gmail.com>
2018-01-15 13:23:44 +0000

    fixed silly bugs!

--- Tutorials/Free-Energy-Framework/simple_bayes.py
+++ Tutorials/Free-Energy-Framework/simple_bayes.py
@@ -29,32 +29,49 @@ def generate_vs(start, stop, step):
 def g(v):
 	return v**2
 
-def calculate_likelihood(v,u):
-	return (1/np.sqrt(2*math.pi)) * np.exp(-(u - g(v)**2)/2)
 
+def calculate_likelihood(v,u):
+	return (1/np.sqrt(2*math.pi)) * np.exp(-((u - g(v))**2)/2)
+
+print calculate_likelihood(np.sqrt(3),3)
 def calculate_prior(v):
 	return (1/np.sqrt(2*math.pi)) * np.exp(-((v-3)**2)/2)
+print calculate_prior(3)
 
 def calculate_unnormalised_posteriors(vs):
 	probs = []
 	for v in vs:
 		prob = calculate_likelihood(v,u) * calculate_prior(v)
-		probs.append(probs)
+		probs.append(prob)
 	return probs
 
 def normalise_probs(probs):
 	probs = np.array(probs)
+	print len(probs)
 	total = sum(probs)
 	normed = []
-	for prob in probs:
-		normed.append(probs/total)
+	for i in xrange(len(probs)):
+		print "adding prob" + str(i) + " out of " + str(len(probs))
+		normed.append(probs[i]/total)
 	normed = np.array(normed)
 	return normed
 
+
+print "starting"
 vs = generate_vs(0.01, 5, 0.01)
+print "generated vs"
 probs = calculate_unnormalised_posteriors(vs)
+print type(probs)
+print len(probs)
+#print probs
+print probs[3]
+print "calculated unnormalised"
 normed = normalise_probs(probs)
-plt.plot(normed, probs)
+print "calculated normalised"
+#p#rint normed
+print probs
+plt.plot(normed)
 plt.show()
-	
+
+# YAY! it works. it can compute this without silly bugs and is awesome. I finally did some straightforward bayesian inference. that's great!!! should be easy, but small steps tbh
 
