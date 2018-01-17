b917cf3467cf780a2859a90d130c87ae3fa5b9fc
Beren <satainvicta@gmail.com>
2018-01-17 17:32:12 +0000

    added a few more gradient update files I think

--- Tutorials/Free-Energy-Framework/pp-layer.py
+++ Tutorials/Free-Energy-Framework/pp-layer.py
@@ -8,6 +8,7 @@
 # I can't believe I'ev spent the whole day on this and have basically nothingto show for it... dagnabbit. The day has gone by so fast. I kind of understand how it works now, and have built the model, but we definitely don't have anything we can apply to autism or whatever for another few days... dagnabbit. and less working time also, I feel!
 
 # I'm really not sure how to build a pp framework. But that's a software challenge for AFTEr I get the autism paper and stuff done, I realy do not know dagnabbit. but I hope it will be good?
+# I mean I'm not any good at software engineering, completely unsurprisingly since I have never done it, and my code is hideous, but trying to write a predictive processing framework for all these people in python will just almost certainly be a mistake lol, but I really do not know to be honest... argh!
 
 import numpy as np
 
@@ -24,6 +25,7 @@ class PPLayer(object):
 		self.weights
 		self.precision
 		self.lrate
+		self.precision_u
 
 
 	#first I think we need to compute our predictions
@@ -35,7 +37,7 @@ class PPLayer(object):
 			return new_pred
 
 	def compute_prediction_errors(self, input_predictions):
-		delta_pred_err = self.predictions - np.dot(self.weights, input_predictions) - np.dot(self.precision, self.pred_errs)
+		delta_pred_err = self.predictions - np.dot(self.weights, input_predictions) - np.dot(np.inv(self.precision), self.pred_errs)
 		new_pred_err = self.pred_errs + (self.lrate * self.delta_pred_err)
 		self.pred_errs = new_pred_err
 		return new_pred_err
@@ -45,6 +47,29 @@ class PPLayer(object):
 
 	def pass_up_prediction_errors(self):
 		return np.dot(self.weights.T, self.pred_errs)
+
+	#compute_perdictions with my formula which I think makes more sense because I cannot understand hwy the book does what it does - look up the diff in the book. has it's own things. Now we need gradient updates
+	def compute_predictions_with_my_formula(self,input_pred_errs):	
+		u_inv = np.inv(self.precision_u)
+		grad = no.dot(self.weights.T, self.activation_function_derivative(self.preds))
+		diff = input_pred_errs - np.dot(self.weights, self.activation_function(self.preds)
+		new_preds = np.dot(grad, np.dot(u_inv, diff)) - self.pred_errs
+		self.preds = new_preds
+		return new_preds
+
+	
+
+	def update_precision(self):
+		delta = 0.5*(np.dot(self.pred_errs.T, self.pred_errs) - np.inv(self.precision))
+		self.precision = self.precision + delta
+		return delta
+
+	def update_weights(self, input_preds):
+		delta = np.dot(self.pred_errs, activation_function(input_preds).T)
+		self.weights += delta
+		return delta
+		
+
 
 
 		
