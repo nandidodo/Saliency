85dff28305f929ce6d5525c5aaa2960ff6c8065a
Beren <satainvicta@gmail.com>
2018-01-17 11:52:59 +0000

    Add files via upload

--- Tutorials/Free-Energy-Framework/network_1.py
+++ Tutorials/Free-Energy-Framework/network_1.py
@@ -24,7 +24,8 @@ sigma_p = 1
 #what order do we evaluate nodes in... who even knows to be honest?
 # I think we should do prediction errors first, but I really don't know
 # do we fix u to a single value? let's define sigma u and p here as simple values
-num_runs = 10
+num_runs = 1000
+lr = 0.01
 
 # and our values so we can plot them
 eps = []
@@ -32,17 +33,21 @@ eus = []
 phis = []
 
 #let's just stat by calculating the errors first and then phi hopefully this will be numeriacally stable and not go crazy
+# okay, well that doesn't work and is not at all numerically stable... dagnabbit!
+#but I haven't actualy implementedthis wrong that I see... f... but it just blows up insanely. dagnabbit!
 
 for i in xrange(num_runs):
 	eps.append(e_p)
-	e_p = phi - v_p - (sigma_p*e_p)
+	e_p = eps[i] + lr*(phi - v_p - (sigma_p*e_p))
 
 	eus.append(e_u)
-	e_u = u - g(phi) - (sigma_u * e_u)
+	e_u = eus[i] + lr*(u - g(phi) - (sigma_u * e_u))
 
 	phis.append(phi)
-	phi = e_u*gdash(phi) - e_p
+	phi = phis[i] + lr*(e_u*gdash(phi) - e_p)
 	print "Epoch: " + str(i)
 	print (phi, e_u, e_p)
+
+# they do phi first as well. let's copy their thing exactly and hope it works. who knew that making numerically stable examples is so difficult!
 
 
