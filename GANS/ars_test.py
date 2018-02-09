1aafb7e0061be8c2e8627f5823a12e57d2d6c97d
Beren <satainvicta@gmail.com>
2018-02-09 16:06:43 +0000

    Add files via upload

--- GANS/ars_test.py
+++ GANS/ars_test.py
@@ -15,22 +15,28 @@ import math
 
 
 input_dims = 1
-intermediate_dim = 5
+intermediate_dim = 10
 output_dims = 1
 def testnet(z):
 	with tf.variable_scope('network'):
-		intermediate_dim = 10
+		#intermediate_dim = 10
 
 		W1 = tf.get_variable('w1', [input_dims, intermediate_dim], initializer=tf.random_normal_initializer(stddev=0.1))
 		B1 = tf.get_variable('b1', [intermediate_dim], initializer=tf.constant_initializer())
 
-		W2 = tf.get_variable('w2', [intermediate_dim, output_dims], initializer=tf.random_normal_initializer(stddev=0.1))
-		B2 = tf.get_variable('b2', [output_dims], initializer=tf.constant_initializer())
+		W2 = tf.get_variable('w2', [intermediate_dim, intermediate_dim], initializer=tf.random_normal_initializer(stddev=0.1))
+		B2 = tf.get_variable('b2', [intermediate_dim], initializer=tf.constant_initializer())
+
+		W3 = tf.get_variable('w3', [intermediate_dim, output_dims], initializer=tf.random_normal_initializer(stddev=0.1))
+		B3 = tf.get_variable('b3', [output_dims], initializer=tf.constant_initializer())
+
 
 		fc1 = tf.nn.relu(tf.matmul(z,W1) + B1)
 		#not sure what activation functoin... I definitely don't want probabilities/sigmoids here
-		fc2 = tf.abs(tf.matmul(fc1, W2) + B2)
-		return fc2 # can't think of anything better to do
+		fc2 = tf.nn.relu(tf.matmul(fc1, W2) + B2)
+		fc3 = tf.abs(tf.matmul(fc2, W3) + B3)
+
+		return fc3 # can't think of anything better to do
 
 		# I'm gonig to try it with 1 D gaussian!
 
@@ -38,9 +44,11 @@ def prob_func(x, params):
 		#do something here to return a vector of correct dim size)
 		#for now try it with a single univariate gaussian
 		#so I just need to implement the gaussian equation here
-		mu, sigma = params
-		return (1/tf.sqrt(2*math.pi * sigma**2)) * tf.exp(-((x-mu)**2)/2*sigma**2)
+		#mu, sigma = params
+		#return (1/tf.sqrt(2*math.pi * sigma**2)) * tf.exp(-((x-mu)**2)/2*sigma**2)
 
+		#let's try a simpler one - a unifomr function
+		return tf.convert_to_tensor(1/20)
 def euclid_distance_loss_func(x,y):
 	#assert len(x) == len(y),'network and probability function have different dimensions!'
 	return tf.sqrt(tf.reduce_sum(x-y)**2)
@@ -54,6 +62,9 @@ def plot_sample_acceptance_rate(samples,bin_width):
 		avgs.append(avg)
 	avgs = np.array(avgs)
 	plt.plot(avgs)
+	plt.title('plot sample acceptance rate over bin widths')
+	plt.xlabel('bin')
+	plt.ylabel('average acceptance rate')
 	plt.show()
 
 def percent_acceptances(acceptances, rejects):
@@ -80,7 +91,7 @@ def train_and_sample():
 	input_dim = 1
 	output_dim = 1
 	num_samples = 100000
-	epochs = 5
+	epochs = 10
 	mu = 0
 	sigma = 1
 	#samples= np.random.uniform(low=-10, high=10, size=num_samples)
@@ -91,6 +102,8 @@ def train_and_sample():
 
 	num_acceptances = 0
 	num_rejections = 0
+
+	lr = 0.0001
 
 	with tf.variable_scope('placeholder'):
 		X = tf.placeholder(tf.float32, [None,1])
@@ -106,7 +119,7 @@ def train_and_sample():
 		loss = euclid_distance_loss_func(testnet(X), prob_func(X, params))
 	
 	with tf.variable_scope('train'):
-		train_step = tf.train.AdamOptimizer().minimize(loss)
+		train_step = tf.train.AdamOptimizer(learning_rate = lr).minimize(loss)
 
 	with tf.variable_scope('prob'):
 		prob_height = prob_func(X, params)
@@ -115,6 +128,7 @@ def train_and_sample():
 	init = tf.global_variables_initializer()
 	sess.run(init)
 	samples = []
+	#nex
 
 
 	for i in xrange(epochs):
@@ -123,9 +137,9 @@ def train_and_sample():
 			height = np.random.uniform(low=0, high=1, size=1)
 			sample = np.reshape(sample,(1,1))
 			result, loss_val, _ ,actual_height= sess.run([res, loss, train_step, prob_height], feed_dict={X: sample})
+			sample_height = result*height
 			print "Epochs: " + str(i) + "runs: " + str(j) + "loss: " + str(loss_val) + " Sample: " + str(sample)
 			print "Result: " + str(result) + "Actual height: " + str(actual_height)
-			sample_height = result*height
 			#now for the sampling step
 			losses.append(loss_val)
 			if sample_height <= actual_height:
@@ -152,8 +166,9 @@ def train_and_sample():
 	print "Percent acceptances: " + str(percent_acceptances(num_acceptances, num_rejections))
 	print "Num acceptances: " + str(num_acceptances)
 	print "Num rejections: " + str(num_rejections)
-	bin_width = 10
+	bin_width = 100
 	plot_sample_acceptance_rate(accept_rejects, bin_width)
+	plot_losses(losses)
 
 
 
