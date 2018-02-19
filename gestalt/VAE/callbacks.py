9d6351abc86ae7c94bbe978a0e4fbfa045d38b9a
Beren <satainvicta@gmail.com>
2018-02-19 13:21:03 +0000

    Add files via upload

--- gestalt/VAE/callbacks.py
+++ gestalt/VAE/callbacks.py
@@ -1,5 +1,6 @@
 import numpy as np
 import keras
+import sys
 
 #Just a simple custom callback for terminating on NaN but when there are multiple losses
 
@@ -19,4 +20,36 @@ class TerminateOnNaNMultipleLosses(keras.callbacks.Callback):
 					NaNDetected = True
 
 		if NaNDetected:
-			self.model.stop_training = true
+			self.model.stop_training = True
+
+#another sinple callback to terminate if the loss increases more than a threshold
+	
+class TerminateOnIncreasedLoss(keras.callbacks.Callback):
+	def __init_(self, diff = 1, monitor="val_loss"):
+		super(TerminateOnIncreasedLoss, self).__init__()
+		self.diff = diff
+		self.monitor=monitor
+		self.wait
+		self.min_loss
+		self.stopped_epoch
+
+	def on_train_begin(self, logs=None):
+		self.wait = 0
+		self.stopped_epoch = 0
+		self.min_loss = np.Inf
+
+	def on_epoch_end(self, epoch, logs=None):
+		current = logs.get(self.monitor)
+		if current + diff > min_loss
+			self.stopped_epochs = epoch
+			print('Loss increased: stopping training')
+			self.model.stop_training= True	
+		else: 
+			self.stopped_epochs = epoch
+			self.min_loss = current
+
+	def on_train_end(self, logs=None):
+		print('Stopped at epochs %d since loss increased' %(self.stopped_epoch))
+
+		
+		
