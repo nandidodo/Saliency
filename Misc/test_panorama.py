78d7d111613727323e4b6bb0216c209e6db98d8a
Beren <satainvicta@gmail.com>
2018-03-02 16:37:28 +0000

    Add files via upload

--- Misc/test_panorama.py
+++ Misc/test_panorama.py
@@ -4,6 +4,7 @@ import cPickle as pickle
 import matplotlib.pyplot as plt
 from scipy.ndimage import imread
 from scipy.misc import imresize
+from panorama import *
 
 
 def save_array(obj, fname):
@@ -14,14 +15,30 @@ def load_array(fname):
 
 
 #get the panorama img
-pan_fname ="./panorama_test_images/016.jpg"
+#pan_fname ="./panorama_test_images/016.jpg"
 #load it
-pan_img = imread(pan_fname)
+#pan_img = imread(pan_fname)
 #save it for later ease of use
-save_fname = "pan_img"
-save_array(pan_img, save_fname)
-plt.imshow(pan_img)
-plt.show()
-print pan_img.shape
+#save_fname = "pan_img"
+#save_array(pan_img, save_fname)
 
+save_fname = "pan_img"
+pan_img = load_array(save_fname)
+#turn to grayscale
+sh = pan_img.shape
+pan_img = np.reshape(pan_img[:,:,0], (sh[0], sh[1]))
+#plt.imshow(pan_img, cmap='gray')
+#plt.show()
+print "Panoramic image shape: " + str(pan_img.shape)
+#now test this
+
+vw = 500
+vh = 500
+#the good thing is it's clear that the zero padding works
+viewport, highlighted_panorama = move_viewport(pan_img, (800, 600), vw, vh, show_viewport=True)
+print viewport.shape
+#plt.imshow(viewport, cmap='gray')
+#plt.show()
+
+show_panorama_and_viewport(highlighted_panorama, viewport)
 
