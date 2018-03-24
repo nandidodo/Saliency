218b626cf458059dd40b8ce594f2e6a4d5722a82
Beren <satainvicta@gmail.com>
2018-03-24 12:20:25 +0000

    Add files via upload

--- RetinalStabilisation/utils.py
+++ RetinalStabilisation/utils.py
@@ -1,5 +1,15 @@
 
 #some simple utils for things
+import numpy as np
+import scipy
+import matplotlib.pyplot as plt
+import cPickle as pickle
+from skimage import exposure
+import sys
+from keras.callbacks import ModelCheckpoint, EarlyStopping, CSVLogger, TensorBoard, TerminateOnNaN, ReduceLROnPlateau
+import os
+
+
 
 def get_error_map(original_img, pred):
 	assert original_img.shape ==pred.shape, 'Original image and prediction must have same shape'
@@ -8,4 +18,47 @@ def get_error_map(original_img, pred):
 
 def get_total_error(error_map):
 	return sum(error_map)
+
+
+
+def save(obj, fname):
+	pickle.dump(obj, open(fname, 'wb'))
+
+def load(fname):
+	return pickle.load(open(fname, 'rb'))
+
+def save_array(obj, fname):
+	pickle.dump(obj, open(fname, 'wb'))
+
+def load_array(fname):
+	return pickle.load(open(fname, 'rb'))
+
+
+def serialize_class_object(f):
+	try:
+		return dict((k,v) for k,v in f.__dict__.iteritems() if not callable(v) and not k.startswith('__'))
+	except Exception as err:
+		print "Exception in Serialization: " + str(err)
+		return {"Error" : err}
+
+
+
+def build_callbacks(save_path, min_delta = 1e-4, patience = 10, histogram_freq=0):
+	
+	checkpointer = ModelCheckpoint(filepath=os.path.join(save_path, "_weights"), monitor="val_loss",save_best_only=True, save_weights_only=True)
+	
+	early_stopper = EarlyStopping(monitor='val_loss', min_delta=min_delta, patience=patience*2)
+
+	epoch_logger = CSVLogger(os.path.join(save_path, "epoch_logs.csv"))
+	
+	#batch_logger= BatchLossCSVLogger(os.path.join(save_path, "batch_logs.csv"))
+	
+	tensorboard = TensorBoard(log_dir=(os.path.join(save_path, '_tensorboard_logs')), histogram_freq=histogram_freq, write_grads=(histogram_freq>0))
+
+	terminator = TerminateOnNaN()
+	
+	reduceLR = ReduceLROnPlateau(monitor='val_loss', factor=0.5, patience= patience, verbose=1, mode='auto', min_lr = 1e-8)
+
+	return [checkpointer, early_stopper, epoch_logger, tensorboard, terminator, reduceLR]
+
 
