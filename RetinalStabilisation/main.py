2d3a57911cdc1b6554c9d8258402c7bba8957990
Beren <satainvicta@gmail.com>
2018-03-25 03:13:59 +0100

    Add files via upload

--- RetinalStabilisation/main.py
+++ RetinalStabilisation/main.py
@@ -19,6 +19,36 @@ from keras import metrics
 # with the data augmentation
 # and also eventually show the decreasing of prediction error
 #with the copied images, so who knows!
+# first I should just check out the overall error there, as far as I know
+
+def run_mnist_model(train, test,save_name=None,epochs=100, Model=SimpleConvDropoutBatchNorm, batch_size = 128, save_model_name=None):
+	#normalise data
+	train = train.astype('float32')/255.
+	test = test.astype('float32')/255.
+
+	#get the model
+	shape = train.shape
+	model = Model(shape[1:])
+	#for now for simplicity
+	model.compile(optimizer='sgd', loss='mse')
+	#callbacks = build_callbacks('/callbacks')
+	his = model.fit(train, train, epochs=epochs, batch_size=batch_size, shuffle=True, validation_data=(test, test))
+	history = serialize_class_object(his)
+
+	#get predictions
+	preds = model.predict(test)
+
+	#save the results
+	if save_model_name is not None:
+		model.save(save_model_name)
+
+	#save the preds and test and history
+	res = [test, preds, his]
+	if save_name is not None:
+		save_array(res,save_name)
+
+	return res
+
 
 
 
@@ -28,6 +58,10 @@ if __name__ == '__main__':
 	NUM_AUGMENTS = 10
 	PIXEL_SHIFT = 4
 	BASE_SAVE_PATH = "data/mnist_dataset"
+	EPOCHS = 1
+	BATCH_SIZE = 64
 	#load the generated dataset
-	augments = np.load(BASE_SAVE_PATH+'_augments')
-	copies = np.load(BASE_SAVE_PATH + '_copies')
+	augments_train = np.load(BASE_SAVE_PATH+'_train_augments')
+	copies_train = np.load(BASE_SAVE_PATH + '_train_copies')
+	augments_test = np.load(BASE_SAVE_PATH+'_train_augments')
+	copies_test = np.load(BASE_SAVE_PATH + '_train_copies')
