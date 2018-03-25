3c7c1c02a4b5fa15750bf4ca37bd7c6ccfd4a978
Beren <satainvicta@gmail.com>
2018-03-25 17:13:53 +0100

    Add files via upload

--- RetinalStabilisation/FixaTons/test.py
+++ RetinalStabilisation/FixaTons/test.py
@@ -19,3 +19,23 @@ fixation_map_matrix = FT.get.fixation_map(DATASET_NAME, STIMULUS_NAME)
 print 'Stimulus dims = ', stimulus_matrix.shape
 print "saliency map dims = ", saliency_map_matrix.shape
 print "fixation map matrix = ", fixation_map_matrix.shape
+
+scanpath = FT.get.scanpath(DATASET_NAME, STIMULUS_NAME, subject = SUBJECT_ID)
+print scanpath.shape
+print scanpath
+print "this scanpath has ", len(scanpath), "fixations."
+
+#show the map
+FT.show.map(DATASET_NAME, STIMULUS_NAME, showSalMap=True, showFixMap=False, wait_time=5000, plotMaxDim=1024)
+
+# that's cool! all of this works even though presumably it requires the cv2 library
+#which I thought Icuoldn't figure out how to install(!)
+
+#show a single scanpath
+FT.show.scanpath(DATASET_NAME, STIMULUS_NAME, subject=SUBJECT_ID,
+	animation=True, wait_time=0, putLines=True, putNumbers = True,
+	plotMaxDim=1024)
+
+
+#okay, that is really really REALLY COOL! it works. That's amazing.
+#The fixatons people did a really good job with this!
