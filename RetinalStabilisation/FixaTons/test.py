46728819d04cdc554f9afc243599467aa23c9a95
Beren <satainvicta@gmail.com>
2018-03-25 19:37:55 +0100

    Add files via upload

--- RetinalStabilisation/FixaTons/test.py
+++ RetinalStabilisation/FixaTons/test.py
@@ -1,3 +1,5 @@
+
+from __future__ import division
 import FixaTons as FT
 import cPickle as pickle
 import numpy as np
@@ -165,7 +167,7 @@ def euclidean_distance_unsafe(p1, p2):
 
 
 # this ignores the timing info. Not sure how useful that is?
-def get_saccade_distances(scanpath_fname, plot=True, save_name=None, info=True):
+def get_saccade_distances(scanpath_fname, plot=True, save_name=None, info=True, return_hist_data=False):
 	scanpaths = load(scanpath_fname)
 	distances = []
 	for scanpath in scanpaths:
@@ -197,7 +199,7 @@ def get_saccade_distances(scanpath_fname, plot=True, save_name=None, info=True):
 	
 	#plot if necessary
 	if plot:
-		plt.hist(distances)
+		n,bins,patches = plt.hist(distances)
 		plt.title('Distribution of distancs between successive scan paths')
 		plt.show()
 
@@ -208,10 +210,13 @@ def get_saccade_distances(scanpath_fname, plot=True, save_name=None, info=True):
 	#vast majority are tiny, but seemingly dwarfed by giant one,
 	#I'm really not sure!
 
+	if return_hist_data:
+		return distances, n,bins,patches
+
 	return distances
 
 
-def get_fixation_durations(scanpath_fname, plot=True, save_name=None, info=True):
+def get_fixation_durations(scanpath_fname, plot=True, save_name=None, info=True, return_hist_data=False):
 	scanpaths = load(scanpath_fname)
 	differences = []
 	for scanpath in scanpaths:
@@ -238,12 +243,15 @@ def get_fixation_durations(scanpath_fname, plot=True, save_name=None, info=True)
 		print "Minumum fixation duration: " , min_duration
 
 	if plot:
-		plt.hist(differences)
+		n, bins, patches = plt.hist(differences)
 		plt.title('Distribution of fixation durations')
 		plt.show()
 
 	if save_name is not None:
 		np.save(save_name, differences)
+
+	if return_hist_data:
+		return differences, n,bins,patches
 
 	return differences
 
@@ -255,7 +263,7 @@ def get_scanpath_durations(scanpath_fname, plot=True, save_name=None, info=True)
 		paths = scanpath['scanpath']
 		length = paths.shape[0]
 		first = paths[0]
-		last = paths[length]
+		last = paths[length-1]
 		differences.append(last[3] - first[2])
 
 	differences = np.array(differences)
@@ -286,10 +294,80 @@ def get_scanpath_durations(scanpath_fname, plot=True, save_name=None, info=True)
 
 
 
+# a simple way to get it is just to plot it on a log log scale,
+# and it shuold be a straight line
+# so what wuold it be - it would be log number of points within the bin
+# vs log bin size = distance betwene points
+
+
+# okay, well figuring these things out is all well and good
+# but in reality  I need specific tests to check the levy flight hypothesis
+# I simply do not know these at the moment, waht I should see
+# or what distributions I shold check
+# even though I actually have a fair amount of statistics now able to be generated
+# about the scanpaths, they still are not that informative to be honest
+# so I honestly do not even know what to look for
+# this is going to require reading some of the papers in detail
+# and trying to understand how they would check
+#whether it is actually a levy flight or not!
+
+def get_middle_of_bins(bins):
+	middles = []
+	for i in xrange(len(bins)-1):
+		start = bins[i]
+		end = bins[i+1]
+		div = (end - start)/2
+		middle = start + div
+		middles.append(middle)
+
+	middles = np.array(middles)
+	return middles
+
+def log_log_plot(x,y, xlabel, ylabel, title):
+	print "In log log plot"
+	print x
+	print y
+	x = np.log(x)
+	y = np.log(y)
+	print x
+	print y
+	fig = plt.figure()
+	plt.plot(x,y)
+	plt.xlabel(xlabel)
+	plt.ylabel(ylabel)
+	plt.title(title)
+	plt.show()
 
 
 
 if __name__ =='__main__':
 	#investigate_scanpath_lengths('fixaton_scanpaths')
 	#distances = get_saccade_distances('fixaton_scanpaths')
-	durations = get_fixation_durations('fixaton_scanpaths')
+	#durations = get_fixation_durations('fixaton_scanpaths')
+	#durations = get_scanpath_durations('fixaton_scanpaths')
+	distances, n,bins,patches = get_saccade_distances('fixaton_scanpaths',return_hist_data=True)
+	print type(n)
+	print len(n)
+	print type(bins)
+	print len(bins)
+	print type(patches)
+	print len(patches)
+	middles = get_middle_of_bins(bins)
+	print type(middles)
+	print len(middles)
+	log_log_plot(middles, n, 'Log Distance between saccades', 'Log Number of saccades','Log log plot of distance between saccades vs number of saccades')
+	# yeah, so far as I can tell, at the moment it doesn't at all follow the log log plot
+	# which is quite bad, though I'm almost certainly doing something wrong
+	# it's obviously not at all a striahgt line, in factthe decrease is probably
+	# greater than the log. dagnabbit. Nevertheless, it's not bad that in a day
+	# I've got some results that either confirm nor deny the hypothesis
+	# which is not at all bad!
+	# I mean I'll need to do some actual tests, but it's not looking good!
+	# but that's actually an interesting result in and of itself
+	# espeically if I can get more data
+	# and beef up the maths and statistics of this
+	# then this could legitimately be a useful result to restart a kind of controversty
+	# and might even get published somewhere
+	# even thoguh it took me just a few days (if that!) to get it sorted
+	# which would not be bad at all!
+
