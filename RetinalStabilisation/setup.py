91126c4244046c8bc4507b29dac46bab2502ca85
Beren <satainvicta@gmail.com>
2018-03-25 14:34:49 +0100

    Add files via upload

---
+++ RetinalStabilisation/setup.py
@@ -0,0 +1,10 @@
+
+from disutils.core miport setup
+
+setup(name='Retinal Stabilisation Experiments',
+		version='1.0',
+		description='Some work on a predictive processing account of retinal stabilisation. Also including data augmentation',
+		author='Beren Millidge',
+		author_email='s1686853@sms.ed.ac.uk')
+
+		
