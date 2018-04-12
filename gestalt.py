8e04eef364d2425129163707bca0f07f905a14fe
Beren <satainvicta@gmail.com>
2018-04-12 22:24:43 +0100

    Add files via upload

--- gestalt.py
+++ gestalt.py
@@ -1,5 +1,3 @@
-# so the aim of this is to do some brief work on gestalts, i.e. for our model we would then split an image in half and try to show some standard gestalt effects from this, whcih seems very reasonable indeed to be honest, but I honestly do not even knwo. we're probably going to majorly focus on this after the first paper is over, but we've got this to do first, so let's look at it
-
 from keras.datasets import cifar10, mnist
 from matplotlib import pyplot as plt
 from scipy.misc import toimage
@@ -22,14 +20,8 @@ import sys
 
 if len(sys.argv) >1:
 	run_num = sys.argv[1]
-# we can then use this globally for the thing when we do our hyperparam grid search
-
 
 def split_dataset_half_small_section(dataset,split_width):
-	#we do this with three dimensions at first, I think, because there's no particular reason to do it with less as we're trying to imitate across the cortex
-	#I think horizontal is just the thing
-	#we assume all images in the dataset are cropped to the same width - a big assumption, so we need that preprocessing step for this to work really
-	img_width = len(dataset[0])
 	shape = dataset.shape
 	print shape
 	half= img_width/2
@@ -49,9 +41,6 @@ def split_dataset_half_small_section(dataset,split_width):
 
 def split_dataset_center_slice(dataset, split_width):
 
-	#this just returns the equivalent of the leftslpit and the rightsplit
-	#also assumes three dimensional images and four d dataset
-	#also all images are cropped to the same width
 	shape = dataset.shape
 	
 	img_width = len(dataset[0])
@@ -123,7 +112,6 @@ def split_half_image_experiments_from_file(fname, epochs=100, save=True, test_up
 	halftrain1, halftrain2 = split_image_dataset_into_halves(train)
 	halftest1, halftest2 = split_image_dataset_into_halves(test)
 	
-	#okay, now we've got our train and test we run the actual experiment
 	if preview:
 		for i in xrange(10):
 			compare_two_images(halftrain1[i], halftrain2[i], reshape=True)
